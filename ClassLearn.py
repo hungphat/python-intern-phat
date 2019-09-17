f6b937f9dbe3b0e97ce352c035627d91cb44a8f4
Phat Future <hungphat0511@gmail.com>
2019-09-17 22:47:27 +0700

    Challenge 2 Class Done

--- ClassLearn.py
+++ ClassLearn.py
@@ -1,14 +1,43 @@
-e=0
 from datetime import date
-# with open("input3.txt") as k:
-#     for test in k:
-#         if e >=2:
-#             abc=test.split()
-#             claim.append(abc[6])
-#         e+=1
-with open("input3.txt") as f:
-    #next(f)
-    for line in f:
-        if e>0:
-            print(line.rstrip())
-        e+=1
+today=date.today()
+class File:
+    def __init__(self, nricfin, first_name, middle_name, last_name, date_of_birth, premium, claim_count):
+        self.nricfin  = nricfin
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.premium = premium
+        self.claim_count = claim_count
+
+    def getFirst_name(self):
+        return self.first_name.capitalize()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------Main------------------
+
+a=File('input3.txt')
+a.create_user_list()
+
+
+
+
